4c59f17ee7dd365738c6bddb8b94fb0b4a7fdbe2
esme <esmeralda.cardona1@catolica.edu.sv>
2024-03-09 08:58:19 -0600

    Linea de conexión a la BD, Creacion del contexto y Modelado de la base de datos creado

---
+++ PARCIAL1A/Models/PARCIAL1AContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace PARCIAL1A.Models
+{
+    public class PARCIAL1AContext : DbContext
+    {
+        public PARCIAL1AContext(DbContextOptions<PARCIAL1AContext> options) : base(options)
+        { }
+
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Libros> Libros { get; set; }
+        public DbSet<Autores> Autores { get; set; }
+        public DbSet<AutorLibro> Equipos { get; set; }
+    }
+}
--- PARCIAL1A/Program.cs
+++ PARCIAL1A/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PARCIAL1A.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Inyección por dependencia del string de conexión al contexto
+
+builder.Services.AddDbContext<PARCIAL1AContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Parcial1ADbConnection")
+        )
+    );
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
